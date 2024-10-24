71add1cb0812878fbe49a9cfc7d3bcb988918a65
Grantsullens <sullens.ga@gmail.com>
2024-10-24 00:50:03 +0000

    Integrated Logic to App

--- app.rb
+++ app.rb
@@ -4,28 +4,53 @@ require "sinatra/reloader"
 
 get("/rock") do
 
-  we_play = "rock"
+  @we_play = "rock"
   they_can_play = ["rock","paper","scissors"]
   @play = rand(0..2)
-  @they_play = they_can_play[play]
+  @they_play = they_can_play[@play]
+  
+  if @they_play == "rock"
+    @outcome = "tied"
+  elsif @they_play == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
   
   erb(:rock)
 end
 
 get("/paper") do
-  we_play = "paper"
+  @we_play = "paper"
   they_can_play = ["rock","paper","scissors"]
   @play = rand(0..2)
-  @they_play = they_can_play[play]
+  @they_play = they_can_play[@play]
+  
+  if @they_play == "rock"
+    @outcome = "won"
+  elsif @they_play == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
   
   erb(:paper)
 end
 
 get("/scissors") do
-  we_play = "scissors"
+  @we_play = "scissors"
   they_can_play = ["rock","paper","scissors"]
   @play = rand(0..2)
-  @they_play = they_can_play[play]
+  @they_play = they_can_play[@play]
+  
+  if @they_play == "rock"
+    @outcome = "lost"
+  elsif @they_play == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
   
   erb(:scissors)
 end
--- test.rb
+++ test.rb
@@ -1,7 +1,13 @@
 we_play = "scissors"
   they_can_play = ["rock","paper","scissors"]
   @play = rand(0..2)
-  @they_play = they_can_play[play]
-
+  @they_play = they_can_play[@play]
+  
   if @they_play == "rock"
-      
+    @outcome = "lost"
+  elsif @they_play == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
